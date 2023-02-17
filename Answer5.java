2348938fc257d5b5f7bf21c736881c747c5ef10f
Prudhvi Raj <118648371+bprudhviraj@users.noreply.github.com>
2023-02-17 14:56:36 +0530

    Update and rename Ansewer5.java to Answer5.java

--- Answer5.java
+++ Answer5.java
@@ -1,12 +1,7 @@
 import java.util.*;
 import java.io.*;
 import java.math.*;
-
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-class Answrer5 {
+class Answer5 {
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
