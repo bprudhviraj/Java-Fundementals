91677817106b82383f198d02cded510a36096375
Prudhvi Raj <118648371+bprudhviraj@users.noreply.github.com>
2023-02-21 17:01:07 +0530

    Finding % between N2 and N1
    01 Test 1
    Input
    Expected output
    3 37
    1
    02 Test 2
    Input
    Expected output
    4 91
    3
    03 Test 3
    Input
    Expected output
    5 156
    1
    04 Test 4
    Input
    Expected output
    13 41
    2

---
+++ Answer10.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+class Solution {
+ public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int N1 = in.nextInt();
+        int N2 = in.nextInt();
+     System.out.println(N2%N1);
+    }
+}
