19b9f903cc2e1efd0597c5263425314463c88914
Prudhvi Raj <118648371+bprudhviraj@users.noreply.github.com>
2023-02-22 12:44:04 +0530

    Create Answer15.java
    Print "Ok" if the super-long integer N is not divisible by four.
    Print "AAAAAAAAAA!!!" otherwise.
    Input
    A super long integer N
    Output
    Ok or AAAAAAAAAA!!!
    Constraints
    1≤ N ≤ 10000000000000000000000000000000000000000000000000000
    000000000000000000000000000000000000000000000000
    Example
    Input
    1
    Output
    Ok

---
+++ Answer15.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+class Answer15{public static void main(String args[]){
+  Scanner in = new Scanner(System.in);
+  long N = in.nextLong();
+  if(N%4==0)
+ {System.out.println("AAAAAAAAAA!!!");}else{System.out.println("Ok");}}}
+  
