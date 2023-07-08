5cdda934611dc12e672e8eb3c04ffa3fe1984bb0
Prudhvi Raj <118648371+bprudhviraj@users.noreply.github.com>
2023-07-08 14:59:20 +0530

    Answer20.java
    //Program to find the Third Angle

---
+++ Answer20.java
@@ -0,0 +1,18 @@
+//Program to find the Third Angle 
+
+
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class Answer20 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c=180-a-b;
+        System.out.println(c);
+    }
+}
